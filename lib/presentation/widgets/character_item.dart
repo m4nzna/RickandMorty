19b4a4c23aedde6934c12e5f93c79de693cf0e1f
m4nza <hamham_b91@hotmail.com>
2026-01-26 20:57:30 -0600

    item

--- lib/presentation/widgets/character_item.dart
+++ lib/presentation/widgets/character_item.dart
@@ -9,22 +9,41 @@ class CharacterItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
+alignment: Alignment.center,
       children: [
-        Image.network(
-          character.image,
-          fit: BoxFit.cover,
-          loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-            if (loadingProgress == null) {
-              return child;
-            }
-            return Center(
-              child: CircularProgressIndicator(
-                value: loadingProgress.expectedTotalBytes != null
-                    ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                    : null,
-              ),
-            );
-          },
+        Card(
+          child:
+        Column(
+          children: [
+            SizedBox(height: 100),
+            Text(character.name),
+            Text(character.species.name),
+
+
+          ],
+        ),
+          ),
+
+          ClipOval(
+
+          child: Image.network(
+            character.image,
+            fit: BoxFit.cover,
+            height: 100,
+            width: 100,
+            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              }
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              );
+            },
+          ),
         ),
 
       ],
