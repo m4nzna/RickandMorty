bc52030933279bc44493d824f8559c6658ab4c33
m4nza <hamham_b91@hotmail.com>
2026-01-27 19:59:29 -0600

    character item

--- lib/presentation/widgets/character_item.dart
+++ lib/presentation/widgets/character_item.dart
@@ -1,70 +1,57 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/models/character_model.dart';
 
 class CharacterItem extends StatelessWidget {
   const CharacterItem({super.key, required this.character});
-  final Result character;
 
+  final Result character;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
-alignment: Alignment.topCenter,
+      alignment: Alignment.topCenter,
       children: [
-
         Column(
           children: [
             SizedBox(height: 20),
             SizedBox(
               width: 150,
 
-              child:
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                SizedBox(height: 80),
-                    Text(character.name, overflow: TextOverflow.ellipsis,
-                      maxLines: 1,),
-                    Text(character.species, overflow: TextOverflow.ellipsis,
-                      maxLines: 1,),
-
-
-                  ],
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 80),
+                      Text(
+                        character.name,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                      Text(
+                        character.species,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-              ),
           ],
         ),
-        ClipOval(
-
-          child: Image.network(
-            character.image,
-            fit: BoxFit.cover,
-            height: 100,
-            width: 100,
-            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              }
-              return Center(
-                child: CircularProgressIndicator(
-                  value: loadingProgress.expectedTotalBytes != null
-                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                      : null,
-                ),
-              );
-            },
-            errorBuilder: (context, error, stackTrace) {
-              return const Icon(Icons.person);
-            },
-          ),
+        CachedNetworkImage(
+          height: 100,
+          width: 100,
+          fit: BoxFit.cover,
+          imageUrl: character.image,
+          placeholder: (context, url) =>
+              Center(child: CircularProgressIndicator()),
+          errorWidget: (context, url, error) => Icon(Icons.error),
+          fadeInDuration: Duration(milliseconds: 300),
         ),
-
-
       ],
     );
   }
